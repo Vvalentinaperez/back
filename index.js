c530a21a903db8ccb5f7078f3ee8b2e5f85475ad
valenpe <valentinaperez230701@gmail.com>
2024-01-08 22:56:55 -0300

    actualizacion del server

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const server = express();
 
-const admin = require("firebase-admin");
-const serviceAccount = require("../back/firebase-admin-credentials/fv-showroom-firebase-adminsdk-z4ssx-abdf682efb.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  // Otras configuraciones si es necesario
-});
-
 const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server listening at  3001`);
