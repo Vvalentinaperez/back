446a7cc7cbf4dd00fb1181c33027dcc78c6a3b6c
valenpe <valentinaperez230701@gmail.com>
2024-02-07 18:47:42 -0300

    union de la base de datos con el back

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const server = express();
-
+const { conn } = require("./src/db.js");
 const PORT = 3001;
-server.listen(PORT, () => {
-  console.log(`Server listening at  3001`);
+
+conn.sync({ alter: false }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server listening at  3001`);
+  });
 });
